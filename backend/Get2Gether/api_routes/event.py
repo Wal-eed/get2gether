91f8fcdc7fd6af909f9ef6a7916a7757cf640dc7
Varun-Sethu <varun.sethu1@education.nsw.gov.au>
2021-04-10 00:52:56 +1000

    added scuffed backend skeleton

--- backend/Get2Gether/api_routes/event.py
+++ backend/Get2Gether/api_routes/event.py
@@ -1,34 +1,108 @@
+from backend.Get2Gether.database.database_util import commit_data_to_key
+import uuid
 from flask import (
     Blueprint,
+    json,
     render_template,
     request,
-    jsonify
+    jsonify,
+    session
 )
 import os
 from Get2Gether.exceptions import InvalidUserInput
 from Get2Gether.utils.colourisation import printColoured
 from Get2Gether.utils.debug import pretty
+from Get2Gether.database import database_util
+
 
 event_router = Blueprint("event", __name__)
+DOMAIN_NAME = "localhost:5000"
 
-@event_router.route("/event", methods=["GET"])
+
+
+# sends and email to all the invitees
+def notify_invitees(invitee_emails: list(str)) -> bool:
+    return True
+
+
+# given a request extracts the free schedule information, this is empty for now coz i have no idea how its gonna work on the frontend
+def get_free_schedule_from_req(req: dict) -> list:
+    pass
+
+
+
+@event_router.route("/event/get", methods=["GET"])
 def get_event():
     """
-        Fetching event/s        
+        Fetches events with a specific event id    
     """
+    requested_id = int(request.args.get("event_id"))
+    event_database = json.loads(
+        database_util.get_json_file("event_data")
+    )
+    if request.args.get("event_id") not in event_database:
+        return "Couldn't find event with ID", 404
+
+    return jsonify(
+        event_database[requested_id]), 200
+
+
+# registration for a specific event
+@event_router.route("/event/register", methods=["POST"])
+def event_register():
+    # just check that user id exists in the session
+    if "user_uid" not in session:
+        return "Not Logged In!", 403
     
-    return jsonify({
-        "some": "event"
+
+    event_id = req.get("event_id")
+    user_data = jsonify({
+        is_orgainser: False,
+        answered_email: True,
+        free_schedule: {
+            get_free_schedule_from_req(req)
+        }
     })
 
-@event_router.route("/event", methods=["POST"])
+    commit_data_to_key("event_data", str(event_id) + str(session["user_id"]), user_data)
+    return jsonify({
+        "event": "registered for event",
+    }), 200
+
+
+@event_router.route("/event/add", methods=["POST"])
 def add_event():
-    """
-        Adding event/s        
-    """
+    # just check that user id exists in the session
+    if "user_uid" not in session:
+        return "Not Logged In!", 403
+    req = request.form
+    event_id = uuid.uuid()
+
+
+    raw_event_data = jsonify({
+        event_name: req.get("event_name"),
+        event_id: event_id,
+        users: {
+            session["user_id"]: {
+                is_orgainser: True,
+                answered_email: True,
+                free_schedule: {
+                    get_free_schedule_from_req(req)
+                }
+            }
+        }
+    })
+    commit_data_to_key("event_data", str(event_id), raw_event_data)
+    # now just email all the invitees with valid emails
+    notify_invitees(json.loads(
+        req.get("atendee_emails")
+    ))
+
 
     return jsonify({
-        "event": "added"
-    })
+        "event": "added",
+        "event_id": event_id,
+        "url": "http://" + DOMAIN_NAME + "/event/join?event_id=" + str(event_id) # i guess the frontend can handle the final link location?
+    }), 200
 
 
--- backend/Get2Gether/api_routes/schedule.py
+++ backend/Get2Gether/api_routes/schedule.py
@@ -2,12 +2,15 @@ from flask import (
     Blueprint,
     render_template,
     request,
-    jsonify
+    jsonify,
+    session
 )
-import os
+import time
 from Get2Gether.exceptions import InvalidUserInput
 from Get2Gether.utils.colourisation import printColoured
 from Get2Gether.utils.debug import pretty
+from Get2Gether.database import database_util
+
 
 schedule_router = Blueprint("schedule", __name__)
 
@@ -23,6 +26,29 @@ not Tuesday!
 
 
 
+# the core of this section, given a user's google key retrieves their calendar and blocked regions in the discussed
+# [0, 1, 1, 0, 1] matrix format
+def get_user_google_calendar(user_google_token: str, week_start: time, week_end: time) -> list(list):
+    # Hardcoding results :)
+    if user_google_token == "abcde123":
+        return [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+    else:
+        return [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0], # basically toy data
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+
+
 
 
 @schedule_router.route("/schedule", methods=["GET"])
@@ -35,27 +61,37 @@ def get_schedule():
             - 
     """
 
-    # 1. find the user with the user_id
+    # retrieve the user's user id from session data
+    # first determine if we have any session data for the user
+    if "user_uid" not in session:
+        return "Not Logged In!", 403
 
-    # 2. pull their calendar data from Google Calendar API
+    user_id = session["user_uid"]
+    user_database = database_util.get_json_file("user_data")
+    current_user_data = user_database.get(user_id)
+    user_presets = current_user_data.get("presets")
+    user_google_calendar = get_user_google_calendar(
+        current_user_data["google_token"], 
+        request.args.get("week_start"), request.args.get("week_end")
+    )
 
-    # 3. pull their active event data (for scheduling with other events)
-    
+
+
     return jsonify({
-        "some": "schedule"
-    })
+        "user_calendar": user_google_calendar,
+        "user_presets": user_presets
+    }), 200
+
 
 @schedule_router.route("/schedule", methods=["POST"])
-def add_schedule():
+def add_user_preset():
     """
-        Adding a user's schedule/s        
+        Adding a preset to the user data     
     """
+    # TODO: fill this in after the creation of an event
 
 
-
-    return jsonify({
-        "schedule": "added"
-    })
+    return "added", 200
 
 
 
