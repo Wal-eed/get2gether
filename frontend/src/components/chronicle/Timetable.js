831e775aa0bc679f7917ffb9b931ed41634150db
Tymotex <timzhang3@gmail.com>
2021-04-10 19:17:09 +1000

    Implemented colour codidng

--- frontend/src/components/chronicle/Timetable.js
+++ frontend/src/components/chronicle/Timetable.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import './Timetable.scss';
+import Button from '../elements/Button';
 
 const days = [
     "",
@@ -40,26 +41,107 @@ const times = [
     "11:00 pm"
 ];
 
+const saveNewEventSchedule = (schedule) => {
+    window.localStorage.setItem("schedule", JSON.stringify(schedule));
+}
 
+const overlayThisSchedule = (schedule) => {
+    const oldSchedule = JSON.parse(window.localStorage.getItem("schedule"));
+    for (let i = 0; i < times.length; i++) {
+        for (let j = 0; j < days.length; j++) {
+            oldSchedule[i][j] += schedule[i][j];
+        }
+    }
+    window.localStorage.setItem("schedule", JSON.stringify(oldSchedule));
+}
 
-const Timetable = props => {
+const Timetable = ({ isOrganiser, defaultSchedule, participants=5, showLegend, modifiable=true, allowAutofill=false }) => {
     const [isMouseDown, setIsMouseDown] = useState(false);
 
+    
     let timeMatrix = [];
     for (let i = 0; i < times.length; i++) {
         timeMatrix[i] = new Array(days.length);
+        for (let j = 0; j < days.length; j++) {
+            timeMatrix[i][j] = 0;
+        }
     }
-    const [schedule, setSchedule] = useState(timeMatrix);
+
+    const unitHue = 255 / participants;
+    console.log(unitHue);
+    const hues = [];
+    const opacities = [];
+    for (let i = 0; i <= participants; i++) {
+        hues.push(i * unitHue);
+        opacities.push(1);
+    }
+    opacities[0] = 0.2;
+    console.log(hues);
+
+    let autofillUnblockedSchedule = [];
+    for (let i = 0; i < times.length; i++) {
+        autofillUnblockedSchedule[i] = new Array(days.length);
+        for (let j = 0; j < days.length; j++) {
+            autofillUnblockedSchedule[i][j] = 0;
+        }
+    }
+
+    // POV: You are blocked on monday, tuesday, wednesday for these times:
+    // Monday 
+    autofillUnblockedSchedule[6][1] = 1;
+    autofillUnblockedSchedule[7][1] = 1;
+    autofillUnblockedSchedule[8][1] = 1;
+    autofillUnblockedSchedule[9][1] = 1;
+    autofillUnblockedSchedule[10][1] = 1;
+    autofillUnblockedSchedule[11][1] = 1;
+    autofillUnblockedSchedule[12][1] = 1;
+    // Tuesday 
+    autofillUnblockedSchedule[11][2] = 1;
+    autofillUnblockedSchedule[12][2] = 1;
+    autofillUnblockedSchedule[13][2] = 1;
+    autofillUnblockedSchedule[14][2] = 1;
+    autofillUnblockedSchedule[15][2] = 1;
+    autofillUnblockedSchedule[16][2] = 1;
+    autofillUnblockedSchedule[17][2] = 1;
+    // Wednesday
+    autofillUnblockedSchedule[11][3] = 1;
+    autofillUnblockedSchedule[12][3] = 1;
+    autofillUnblockedSchedule[13][3] = 1;
+    autofillUnblockedSchedule[14][3] = 1;
+    autofillUnblockedSchedule[15][3] = 1;
+    autofillUnblockedSchedule[16][3] = 1;
+    autofillUnblockedSchedule[17][3] = 1;
+
+    const autofill = () => {
+        for (let i = 0; i < times.length; i++) {
+            for (let j = 0; j < days.length; ++j) {
+                if (autofillUnblockedSchedule[i][j]) {
+                    // This time is blocked. Do not add
+                    schedule[i][j] = 0;
+                } else {
+                    schedule[i][j] = 1;
+                }
+            }
+        }
+        setSchedule([...schedule]);
+    }
+
+
+    const [schedule, setSchedule] = useState(defaultSchedule ? defaultSchedule : timeMatrix);
 
     const handleSelectTime = (row, col, force=false) => {
-        if (isMouseDown || force) {
-            console.log("Selecting!!!" + row + " " + col);
-            let newTimeMatrix = [];
-            for (let i = 0; i < times.length; i++) {
-                newTimeMatrix[i] = [...schedule[i]];            
+        if (modifiable) {
+            if (isMouseDown || force) {
+                let newTimeMatrix = [];
+                for (let i = 0; i < times.length; i++) {
+                    newTimeMatrix[i] = [...schedule[i]];            
+                }
+                newTimeMatrix[row][col] = (newTimeMatrix[row][col]) ? (0) : (1);
+                setSchedule(newTimeMatrix);
+                if (isOrganiser) {
+                    saveNewEventSchedule(newTimeMatrix);
+                }
             }
-            newTimeMatrix[row][col] = (newTimeMatrix[row][col]) ? (0) : (1);
-            setSchedule(newTimeMatrix);
         }
     };
 
@@ -84,13 +166,21 @@ const Timetable = props => {
                                 </td>
                                 {days.map((_, col) => {
                                     if (col !== 0) {
+                                        // if (schedule[row][col]) console.log("1");
+                                        // console.log(schedule[row][col]);
                                         return (
                                             <td 
                                                 // id={`time-${row}-${col}`} 
                                                 className={`time-brick noselect ${schedule[row][col] && "time-brick-selected"}`} 
+                                                style={
+                                                    (!modifiable) ? ({
+                                                        backgroundColor: `rgba(0, 0, ${hues[schedule[row][col]]}, ${opacities[schedule[row][col]]})`
+                                                    }) : ({})
+                                                }
                                                 onMouseOver={() => handleSelectTime(row, col)}
                                                 onMouseDown={() => handleSelectTime(row, col, true)}
                                             >
+                                                {schedule[row][col]}
                                             </td>
                                         );
                                     }
@@ -100,6 +190,28 @@ const Timetable = props => {
                     </tbody>
                 </table>
             </div>
+            {allowAutofill && (
+                <>
+                    <Button color="primary" onClick={autofill}>Auto-Fill</Button> 
+                    <Button color="info" onClick={() => overlayThisSchedule(schedule)}>Save</Button> 
+                </>
+            )}
+            {showLegend && (
+                <div>
+                    <h4 style={{textAlign: "left"}}>
+                        Legend:
+                    </h4>
+                    <div style={{width: (participants * 40) + "px", backgroundColor: "whitesmoke", height: "40px", borderRadius: "10px"}}>
+                        {Array.from(Array(participants).keys()).map(i => {
+                            return (
+                                <div style={{display: "inline-block", width: "32px", margin: "4px", height: "30px", backgroundColor: `rgb(0, 0, ${hues[i]})`, borderRadius: "10px"}}>
+                                    {i+1}/{participants}
+                                </div>
+                            )
+                        })}
+                    </div>
+                </div>
+            )}
         </div>
     )
 }
--- frontend/src/components/sections/Hero.js
+++ frontend/src/components/sections/Hero.js
@@ -61,7 +61,7 @@ const Hero = ({
    */
   const [page, setPage] = useState("start");
   const [meetingName, setMeetingName] = useState(null);
-
+  const [isOrganiser, setIsOrganiser] = useState(null);
 
   const startNewMeeting = (event) => {
     event.preventDefault();
@@ -78,61 +78,89 @@ const Hero = ({
       <div className="container-sm">
         <div className={innerClasses}>
           <div className="hero-content">
-                
-            {(page === "start") && (
-              <FadeIn
-                delay={600}
-                transitionDuration={1000}
-              >
-                {/* <Ass /> */}
-                <h1>
-                  Get2Gether 🗨️
-                </h1>
-                <div className="container-xs">
-                  <p>
-                    "When2Meet, <strong><em style={{fontFamily: "Garamond"}}>but not shit</em></strong>."
-                  </p>
-                  <div style={{marginTop: "200px"}}>
-                    <SearchField startNewMeeting={startNewMeeting} />
-                  </div>
-                </div>
-              </FadeIn>
-            )}
-            
-            
 
-            {(page === "day-selection") && (
-              <FadeIn
-                delay={400}
-                transitionDuration={1000}
+            {isOrganiser === null && (
+              <>
+                <div>
+                  Please select what you are:
+                </div>
+                <div>
+                  <Button color="primary" onClick={() => setIsOrganiser(true)} style={{"margin": "10px"}}>Organiser</Button>
+                  <Button color="primary" onClick={() => setIsOrganiser(false)} style={{"margin": "10px"}}>Attendee</Button>
+                </div> 
+              </>
+            )}
+            {/* ATTENDEE SCREENS */}
+            {isOrganiser === false && (
+                <FadeIn
+                // delay={400}
+                // transitionDuration={1000}
               >
-                <h1>
-                  Select days for <span className="glow">
-                    {meetingName}
-                  </span>
-                </h1>
-                <Button color="primary" onClick={() => setPage("start")}>Back</Button>
-                <Button color="primary" onClick={() => setPage("timetable")}>Next</Button>
+                <h3>Your Availability</h3>
+                <Timetable isOrganiser={isOrganiser} allowAutofill={true} />
+
+                <hr />
+                <h3>Everyone's Availabilities</h3>
+                <Timetable defaultSchedule={JSON.parse(window.localStorage.getItem("schedule"))} isOrganiser={isOrganiser} participants={5} showLegend={true} modifiable={false} />
               </FadeIn>
             )}
-            {(page === "timetable") && (
-              <FadeIn
-                delay={400}
-                transitionDuration={1000}
-              >
-                <Timetable />
-                <Button color="primary" onClick={() => setPage("day-selection")}>Back</Button>
-                <Button color="primary" onClick={() => setPage("invite")}>Next</Button>
-              </FadeIn>
+            {/* ORGANISER SCREENS */}
+            {isOrganiser === true && (
+              <>
+                {(page === "start") && (
+                  <FadeIn
+                    delay={600}
+                    transitionDuration={1000}
+                  >
+                    <h1>
+                      Get2Gether 🗨️
+                    </h1>
+                    <div className="container-xs">
+                      <p>
+                        "When2Meet, <strong><em style={{fontFamily: "Garamond"}}>but not shit</em></strong>."
+                      </p>
+                      <div style={{marginTop: "200px"}}>
+                        <SearchField startNewMeeting={startNewMeeting} />
+                      </div>
+                    </div>
+                  </FadeIn>
+                )}
+                {(page === "day-selection") && (
+                  <FadeIn
+                    delay={400}
+                    transitionDuration={1000}
+                  >
+                    <h1>
+                      Select days for <span className="glow">
+                        {meetingName}
+                      </span>
+                    </h1>
+                    <Button color="primary" onClick={() => setPage("start")}>Back</Button>
+                    <Button color="primary" onClick={() => setPage("timetable")}>Next</Button>
+                  </FadeIn>
+                )}
+                {(page === "timetable") && (
+                  <FadeIn
+                    delay={400}
+                    transitionDuration={1000}
+                  >
+                    <Timetable isOrganiser={isOrganiser} />
+                    <Button color="primary" onClick={() => setPage("day-selection")}>Back</Button>
+                    <Button color="primary" onClick={() => setPage("invite")}>Next</Button>
+                  </FadeIn>
+                )}
+                {(page === "invite") && (
+                  <FadeIn
+                    delay={400}
+                    transitionDuration={1000}
+                  >
+                    <Button color="primary" onClick={() => setPage("start")}>Back to start</Button>
+                    <Button color="primary">View Responses</Button>
+                  </FadeIn>
+                )}
+              </>
             )}
-            {(page === "invite") && (
-              <FadeIn
-                delay={400}
-                transitionDuration={1000}
-              >
-                <Button color="primary" onClick={() => setPage("start")}>Back to start</Button>
-              </FadeIn>
-            )}
+                
           </div>
         </div>
       </div>
